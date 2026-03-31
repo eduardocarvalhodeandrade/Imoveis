722bebd5086f6c8357a6973fc3ca338fd7ad903c
eduardocarvalhodeandrade <eduardocarvalhodeandrade2@gmail.com>
2026-03-30 22:01:33 -0300

    Comparação descritiva do Preço x metragem

--- Analise.R
+++ Analise.R
@@ -1,4 +1,17 @@
+library(ggplot2)
+
 dados = read.csv("imoveis.csv", header = T, sep = ";")
 attach(dados)
 
+#Obrigatorios
 hist(preco, breaks = 6, main = "Preço", xlab = "R$", col = "black")
+ggplot(data = dados, aes(x = imposto_anual, y = metragem)) + geom_point() + geom_smooth(method = "lm", se = FALSE)
+
+#Opcionais
+mean(itens_embutidos)
+sd(itens_embutidos)
+
+mean(imposto_anual)
+sd(imposto_anual)
+
+ggplot(data = dados, aes(x = idade, y = metragem)) + geom_point() + geom_smooth(method = "lm", se = FALSE)
