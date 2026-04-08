435aaa0fbe9f839a220d6c17c527e2c107e0069d
= <=>
2026-04-08 10:51:04 -0300

    Script esqueleto

---
+++ imoveis-com-branches.R
@@ -0,0 +1,17 @@
+#Script esqueleto.
+#1-Leitura do banco de dados
+Imoveis = read.csv("imoveis.csv", header=T, sep=";")
+str(Imoveis)
+head(Imoveis)
+#Existe uma linha em que o Preço da Imovel é menor do que o imposto anual, interpreto isso como um erro, então vou remover.
+Imoveis$preco[Imoveis$preco < Imoveis$imposto_anual] = NA
+Imoveis$imposto_anual[Imoveis$preco < Imoveis$imposto_anual] = NA
+ImoveisLimpos = na.omit(Imoveis)
+#2- Gráficos
+
+#insira Gráficos aqui.
+
+#3- Medidas
+
+#insira Medidas aqui.
+
